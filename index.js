27f453d7566497da298bbdbf45ba51e6ae66f0a0
Stephen Flores <sflores813@gmail.com>
2020-10-15 01:25:10 -0500

    quick edit

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 require("console.table")
-require("dotenv").config();
+
 
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: process.env.MYSQL_PW,
+    password:"",
     database: "employee_databaseDB"
 });
 //starts connection to server
