0404bd3571b8bbaf21df0eba02c1e0e78a9b3094
Stephen Flores <sflores813@gmail.com>
2020-10-16 11:32:34 -0500

    working on the plane home

--- index.js
+++ index.js
@@ -84,22 +84,95 @@ function start() {
     };
 
     // function to add role data
-
+    function addRole() {
+        inquirer.prompt([{
+            type: " input",
+            name: "title",
+            message: "What is the employee's role?:",
+        }, {
+            type: "number",
+            name: "salary"
+            message: "What is the employees salary?",
+        },
+            type:    
+        }])
+    }
 
     //function to add employee data
+    funtion addEmployee() {
+        inquirer.prompt([{
+        name:"firstname",
+        type: "input",
+        message: "What is the Employees name?",
+        }, {
+        name:"lastname",
+        type: "input",
+        message: "What is the Employees last name?",
+        }, {
+        name: "role",
+        type:"input",
+        message:"Whats is the employee role?"    
+        }, {
+        name: "ID",
+        type:"number",
+        message:"Whats is the employee ID number?"    
+        }, {
+        name: "Employee Manager",
+        type:"input",
+        message:"Who is the employee's manager?"    
+        },
+     ])
+     .then(function (answer) {
+        //inserts the entered employee into data base
+        connection.query(
+            "INSERT INTO employee SET ?", {
+                first_name: answer.firstname,
+                last_name: answer.last_name,
+                role: answer.role,
+                ID: answer.ID,
+                Employee_Manager: answer.Employee_Manager
+            },
+            function (err) {
+                if (err) throw err;
+                console.table(`${answer.firstName} ${answer.last_name} added successfully`)
+                start();
+            }
+        );
+     });
+    }
+        
 
-    
 
     // function to View role
 
-
+function viewRoles() {
+    connection.query("SELECT * FROM role" , function(err,res) {
+        console.table(res);
+        if (err) throw err;
+        start();
+    });
+};
 
     // function to View employee
+
+    connection.query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id",")
+    function(err, res) {
+        console.table(res);
+        if(err) throw err;
+        start();
+    });
 
 
 
     // function to View department
 
+function ViewDepartment() {
+    connection.query("SELECT * FROM department", function(err, res){
+        console.table(res),
+        if(err); throw err;
+        start();
+    });
+};
 
 
     // function to Update employee
